7f5c3a7275f541be51643743d998bda447692229
jenuk <doctor_who_1996@mail.ru>
2024-10-02 00:48:52 +0500

    fixt masks.py, widget.py

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,25 @@
 def get_mask_card_number(card_numbers: str) -> str:
     """Функция, скрывающая номер карты"""
     card_number = str(card_numbers)
+    if card_number.isdigit():
+        if len(card_number) < 16:
+            return ("Ошибка ввода")
+        elif len(card_number) > 16:
+            return "Ошибка ввода"
 
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    else:
+        return "Ошибка ввода"
 
 
 def get_mask_account(account_numbers: str) -> str:
     """Фунцкия, скрывающая номер банковского счета"""
     account_number = str(account_numbers)
-    return "**" + account_number[-4:]
+    if account_number.isdigit():
+        if len(account_number) < 20:
+            return "Ошибка ввода"
+        elif len(account_number) > 20:
+            return "Ошибка ввода"
+        return "**" + account_number[-4:]
+    else:
+        return "Ошибка ввода"
--- src/widget.py
+++ src/widget.py
@@ -5,24 +5,26 @@ def mask_account_card(account_card: str) -> str:
     """Функция, которая умеет обрабатывать информацию как о картах, так и о счетах"""
     count_number = ""
     name_card = ""
-
     for element in account_card:
         if element.isdigit():
             count_number += str(element)
         else:
             name_card += str(element)
-    if len(count_number) == 16:
-        card_mask = masks.get_mask_card_number(count_number)
-        return f"{name_card} {card_mask}"
-    elif len(count_number) == 20:
-        account_mask = masks.get_mask_account(count_number)
-        return f"{name_card} {account_mask}"
+    if len(count_number) == 0:
+        return "Ошибка ввода"
+    elif len(count_number) == 16 or len(count_number) == 20:
+        if len(count_number) == 16:
+            card_mask = masks.get_mask_card_number(count_number)
+            return f"{name_card} {card_mask}"
+        elif len(count_number) == 20:
+            account_mask = masks.get_mask_account(count_number)
+            return f"{name_card} {account_mask}"
+    else:
+        return "Ошибка ввода"
 
+print(mask_account_card("Cxtn 213512"))
 
-print(mask_account_card("Счет 73654108430135874305"))
-
-
-def get_date(user_date: str) -> str:
+def get_data(user_date: str) -> str:
     """Функция принимающая строку даты и выводящая дату в формате дд.мм.гггг"""
     user_data_slace = user_date[0:10]
     revers_user_data = f"{user_data_slace[-2::1]}. {user_data_slace[-5:-3]}. {user_data_slace[0:4]}"
