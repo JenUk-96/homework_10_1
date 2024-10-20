e525235c22baf9e972feebd49763bef80a7ce033
jenuk <doctor_who_1996@mail.ru>
2024-10-20 22:27:10 +0500

    hw_utils

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from src import widget
+from src import external_api
+from src.external_api import convert_to_rub
 
 print(widget.mask_account_card('Visa Platinum 7000792289606361'))
 
 print(widget.mask_account_card('Счет 73654108430135874305'))
 
 print(widget.get_data('2024-03-11T02:26:18.671407'))
+
+print(convert_to_rub('data\operations.json'))
